16973a8b65d237f0576c9ad16ceea37043e3ec13
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-10-14 23:40:47 +0300

    Update mix.py

--- mix.py
+++ mix.py
@@ -1,5 +1,6 @@
 import random
 gamers = dict()
+gamersList = ("gamer1", "gamer2", "gamer3", "gamer4")
 stone = ["SI1", "SI2", "SI3", "SI4", "SI5", "SI6", "SI7", "SI8", "SI9", "SI10", "SI11", "SI12", "SI13",
          "MA1", "MA2", "MA3", "MA4", "MA5", "MA6", "MA7", "MA8", "MA9", "MA10", "MA11", "MA12", "MA13",
          "KI1", "KI2", "KI3", "KI4", "KI5", "KI6", "KI7", "KI8", "KI9", "KI10", "KI11", "KI12", "KI13",
@@ -18,14 +19,14 @@ def mix():
         test = "{}{}".format(test[0:2], int(test[2:]) + 1)
         fakestone = test
     stones = random.sample(stone, len(stone))
-    startergamer = random.choice([0,1,2,3]) #oyunu başlatan oyuncu
-    print "oyunu", "{}.oyuncu baslatiyor".format(startergamer+1)
-    for deal, gamer in zip([0,15,30,45], ("gamer1", "gamer2", "gamer3", "gamer4")):
-        if deal is [0,15,30,45][startergamer]: 
+    startergamer = random.choice(gamersList) #oyunu başlatan oyuncu
+    print "oyunu", "{} baslatiyor".format(startergamer)
+    for gamer in gamersList:
+        if startergamer is gamer: 
             total = 15 #taş sayısı
         else:
             total = 14
-        gamers[gamer] = stones[deal:total+deal]
+        gamers[gamer] = stones[0:total]
         del stones[0:total]
     return {"joker":joker, 
             "fakestone":fakestone,
